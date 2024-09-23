1fe042bf4f565aff780e1dd9ecc8f8f310f55b4e
Zoorphy <116314114+jeansouza1994@users.noreply.github.com>
2024-09-23 16:26:02 -0300

    add-tooltip

---
+++ script.js
@@ -0,0 +1,15 @@
+// Manipulação da Tooltip
+
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// Manipulção do Slide
+var myCarousel = document.querySelector("#carouselExampleIndicators");
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000,
+    wrap: false,
+});
